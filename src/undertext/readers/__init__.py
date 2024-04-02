a6202ec69b428636e0a44d5b6e52bb4748c0ef2f
playerg9 <g9yout@gmail.com>
2024-04-02 15:18:04 +0200

    added undertext.{readers,writers}

---
+++ src/undertext/readers/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import typing as t
+from os import PathLike
+from pathlib import Path
+from ..structures import Caption
+# from .microdvd import read_microdvd
+# from .subrip import read_subrip
+# from .webvtt import read_webvtt
+
+
+EXT_MAP = dict(
+    # srt=read_subrip,
+    # sub=read_microdvd,
+    # vtt=read_webvtt,
+)
+ALIAS_MAP = dict(
+    # microdvd=read_microdvd,
+    # subrip=read_subrip,
+    # webvtt=read_webvtt,
+)
+
+
+def loads(fp: t.Union[str, PathLike], fmt: str = None, **kwargs) -> t.List[Caption]:
+    r"""
+    load the captions from a file
+
+    :param fp: subtitle file to load
+    :param fmt: specific format name
+    :param kwargs: optional arguments some formats may need
+    :return: Caption[]
+    """
+    fp = Path(fp)
+
+    if fmt is None:
+        fmt = fp.suffix[1:]
+        reader = EXT_MAP.get(fmt.lower())
+    else:
+        reader = ALIAS_MAP.get(fmt.lower())
+
+    if reader is None:
+        raise ValueError(f"Unknown format ({fmt!s})")
+
+    return reader(fp, **kwargs)
---
+++ src/undertext/writers/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import typing as t
+from os import PathLike
+from pathlib import Path
+from ..structures import Caption
+# from .microdvd import write_microdvd
+# from .subrip import write_subrip
+# from .webvtt import write_webvtt
+
+
+EXT_MAP = dict(
+    # srt=write_subrip,
+    # sub=write_microdvd,
+    # vtt=write_webvtt,
+)
+ALIAS_MAP = dict(
+    # microdvd=write_microdvd,
+    # subrip=write_subrip,
+    # webvtt=write_webvtt,
+)
+
+
+def dumps(captions: t.List[Caption], fp: t.Union[str, PathLike], fmt: str = None, **kwargs) -> None:
+    r"""
+    dumps some captions into a file
+
+    :param captions: captions to dump
+    :param fp: subtitle file to dump into
+    :param fmt: specific format name
+    :param kwargs: optional arguments some formats may need
+    """
+    fp = Path(fp)
+
+    if fmt is None:
+        fmt = fp.suffix[1:]
+        writer = EXT_MAP.get(fmt.lower())
+    else:
+        writer = ALIAS_MAP.get(fmt.lower())
+
+    if writer is None:
+        raise ValueError(f"Unknown format ({fmt!s})")
+
+    return writer(captions, fp, **kwargs)
