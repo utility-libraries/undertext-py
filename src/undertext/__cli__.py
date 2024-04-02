06a8883f9f2814e7ca6147d42b6e7af36286d69b
playerg9 <g9yout@gmail.com>
2024-04-02 15:18:48 +0200

    implemented CLI

---
+++ src/undertext/__cli__.py
@@ -0,0 +1,33 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+from pathlib import Path
+from . import loads, dumps
+
+
+def cmd_read(input_fp, **kwargs) -> None:
+    input_fp = Path(input_fp)
+    if not input_fp.is_file():
+        raise FileNotFoundError(str(input_fp))
+
+    for caption in loads(input_fp, **kwargs):
+        print(f"{caption!r}")
+
+
+def cmd_convert(input_fp: str, output_fp: str, overwrite: bool = False, **kwargs):
+    input_fp = Path(input_fp)
+    if not input_fp.is_file():
+        raise FileNotFoundError(str(input_fp))
+    output_fp = Path(output_fp)
+    if output_fp.is_file():
+        if not overwrite:
+            raise FileExistsError(str(output_fp))
+    elif output_fp.exists():
+        raise RuntimeError(f"{output_fp!s} can't be overwritten")
+
+    input_kwargs = {key[6:]: value for key, value in kwargs.items() if key.startswith("input_")}
+    output_kwargs = {key[7:]: value for key, value in kwargs.items() if key.startswith("output_")}
+
+    captions = loads(input_fp, **input_kwargs)
+    dumps(captions, output_fp, **output_kwargs)
---
+++ src/undertext/__main__.py
@@ -0,0 +1,53 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import argparse as ap
+from . import __cli__ as cli
+
+
+parser = ap.ArgumentParser(prog="undertext", formatter_class=ap.ArgumentDefaultsHelpFormatter)
+parser.set_defaults(fn=parser.print_help)
+subparsers = parser.add_subparsers()
+
+read_parser = subparsers.add_parser("read",
+                                    epilog="Note: Some optional arguments are hidden")
+read_parser.set_defaults(fn=cli.cmd_read)
+read_parser.add_argument("input_fp", metavar="input",
+                         help="input file")
+read_parser.add_argument("--fmt", type=str, default=ap.SUPPRESS,
+                         help="specify the format if it can't be guessed from the file extension")
+read_parser.add_argument("--fps", type=float, default=ap.SUPPRESS,
+                         help=ap.SUPPRESS)
+
+convert_parser = subparsers.add_parser("convert",
+                                       epilog="Note: Some optional arguments are hidden and need to be prefixed"
+                                              " with '--input-' or '--output-'")
+convert_parser.set_defaults(fn=cli.cmd_convert)
+convert_parser.add_argument("input_fp", metavar="input",
+                            help="input file")
+convert_parser.add_argument("output_fp", metavar="output",
+                            help="output file")
+convert_parser.add_argument("-o", '--overwrite', action=ap.BooleanOptionalAction, default=False,
+                            help="overwrite existing output file")
+
+convert_parser.add_argument("--input-fmt", type=str, default=ap.SUPPRESS,
+                            help="specify the input format if it can't be guessed from the file extension")
+convert_parser.add_argument("--output-fmt", type=str, default=ap.SUPPRESS,
+                            help="specify the output format if it can't be guessed from the file extension")
+
+# next two are for microdvd
+convert_parser.add_argument("--input-fps", type=float, default=ap.SUPPRESS,
+                            help=ap.SUPPRESS)
+convert_parser.add_argument("--output-fps", type=float, default=ap.SUPPRESS,
+                            help=ap.SUPPRESS)
+
+
+def main(args=None):
+    arguments = vars(parser.parse_args(args))
+    fn = arguments.pop('fn')
+    return fn(**arguments)
+
+
+if __name__ == "__main__":
+    main()
